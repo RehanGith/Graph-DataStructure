657dcbcaa12c5657b5e2e971c49c99f5001786e0
RehanGith <chrehan1007@gmail.com>
2024-05-30 20:47:26 +0500

    Created a graph class

--- Graph/main.cpp
+++ Graph/main.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
+#include <vector>
 using namespace std; 
 
+class Graph {
+	vector<vector<int>> matrix;
+	int n;
+public:
+	Graph(int num = 0, int num2 = 0) : n(num + 1) {
+		matrix.resize(n, vector<int>(n,  0));
+	}
+	void display() {
+		if (n == 0) {
+			cout << "Graph is empty" << endl;
+		}
+		for (int i = 1; i < n; i++) {
+			for (int j = 1; j < n; j++) {
+				cout << matrix[i][j] << " , ";
+			}
+			cout << endl;
+		}
+	}
+};
 int main() {
-
+	Graph g1(7, 7);
+	g1.display();
 	return 0;
 }
